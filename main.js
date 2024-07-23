f3335efc8f033f77450da54ff6dbd0f8b07fbd26
mohamedzenaidi19 <mohamedzenaidi21@gmail.com>
2024-07-23 09:30:37 +0100

    hh

---
+++ main.js
@@ -0,0 +1,37 @@
+const counter=document.getElementById("counter") 
+const incr =document.querySelector(".incr") 
+const decr=document.querySelector(".decr") 
+
+var count=0; 
+incr.addEventListener("click,function("){ 
+    count++
+    counter.innerHTML=count 
+    } ) 
+    decr.addEventListener("click,function"(){ 
+        if (count>0){ 
+            count--
+            counter.innerHTML=count 
+    }  
+   }) 
+   const counter=document.getElementById("counter") 
+const incr =document.querySelector(".incr") 
+const decr=document.querySelector(".decr")
+       log(...data:any[]) 
+       console.log("counter")
+       console.log(".incr")
+       console.log(".decr") 
+       var count=0; 
+incr.addEventListener("click,function("){ 
+    count++
+    counter.innerHTML=count 
+    } ) 
+    decr.addEventListener("click,function"(){ 
+        if (count>0){ 
+            count--
+            counter.innerHTML=count 
+          } else( 
+            return 
+           }  
+           ] ) 
+
+
